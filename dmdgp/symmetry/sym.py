937ec3d040310e634899f3ba05257b28c4916e65
Andrês <andresroliveira98@gmail.com>
2022-08-04 17:48:33 -0300

    Update sym.py

--- dmdgp/symmetry/sym.py
+++ dmdgp/symmetry/sym.py
@@ -13,6 +13,29 @@ def sym(g: Graph) -> list:
 
     eps = [(i, m[i]) for i in range(n + 1) if m[i] != 0]
 
-    print(eps)
+    s_bar = []
 
-    return []
+    if eps:
+        l, r = eps[0]
+
+        for vi, vmi in eps:
+            if r >= vi + 3:
+                r = max(vmi, r)
+            else:
+                s_bar.append((l + 4, r))
+                l = vi
+                r = vmi
+        s_bar.append((l + 4, r))
+
+    s = [4]
+    i = 0
+
+    for v in range(5, n + 1):
+        u_bar, v_bar = s_bar[i]
+
+        if u_bar > v or v > v_bar:  # if v \not \in [u_bar, v_bar]
+            s.append(v)
+
+        if v == v_bar and i + 1 < len(s_bar): i += 1
+
+    return s
