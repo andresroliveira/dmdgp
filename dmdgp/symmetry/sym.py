4a2a2f53701407fc73c1c4340b6c1b05fa6bbe6a
Andrês <andresroliveira98@gmail.com>
2022-08-04 17:28:26 -0300

    Update sym.py

--- dmdgp/symmetry/sym.py
+++ dmdgp/symmetry/sym.py
@@ -1,2 +1,18 @@
-def sym():
-    return
+import numpy as np
+from ..Graph import Graph
+
+
+def sym(g: Graph) -> list:
+    n = int(g)
+    prune = g.prune_edges()
+
+    m = [0 for _ in range(n + 1)]
+
+    for u, v in prune:
+        m[u] = max(m[u], v)
+
+    eps = [(i, m[i]) for i in range(n + 1) if m[i] != 0]
+
+    print(eps)
+
+    return []
